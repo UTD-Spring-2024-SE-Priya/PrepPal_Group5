924d4f6165a78ff54260091eb67b006b13198795
msiddique12 <67293796+msiddique12@users.noreply.github.com>
2024-03-29 17:55:28 -0500

    Added print statements for the tests

--- Server/src/test/java/com/group5/PrepPal/UserTest.java
+++ Server/src/test/java/com/group5/PrepPal/UserTest.java
@@ -11,38 +11,44 @@ import static com.mongodb.assertions.Assertions.assertNull;
 public class UserTest {
     public static void main(String[] args){
 
-        System.out.println("Hello, World!");
+        System.out.println("Running all test cases!");
+
         // testcase (username = invalid)
         User tester = new User("4000", "zen.yamaguchi168@gmail.com", "buh", "12345678");
         assertNull(tester.getUsername());
+        System.out.println("Test case 1 passed.");
 
 
         //test case (user name = exceptional)
         User test1 = new User("4009900", "biz.yamaguchi168@gmail.com", "buh", "123485678");
          assertNull(test1.getUsername());
+        System.out.println("Test case 2 passed.");
 
         // test case(username = valid)
         User test2 = new User("4008800", "wiz.yamaguchi168@gmail.com", "bruh", "1234856378");
         assertNotNull(test2.getUsername());
-
-
+        System.out.println("Test case 2 passed.");
 
         // test case(email = not valid)
         User test3 = new User("4001100", "wiz.yamaguchi168gmail", "bruh", "1234856378");
-
          assertNull(test3.getUsername());
+        System.out.println("Test case 3 passed.");
 
         // test case (email = valid)
         User test4 = new User("40020", "zen.yamaguchi168@gmail.com", "testing", "12232!345678");
         assertNotNull(test4.getUsername());
+        System.out.println("Test case 4 passed.");
 
         //test case(password = not valid)
         User test5 = new User("400500", "w213z.yamaguchi168@gmail", "tester", "1");
         assertNull(test5.getUsername());
+        System.out.println("Test case 5 passed.");
 
         //test case (password = valid)
         User test6 = new User("40088030", "wiz.yamaguchi168@gmail.com", "bruh", "1234856378");
         assertNotNull(test6.getUsername());
+        System.out.println("Test case 6 passed.");
+        System.out.println("All test cases passed!");
     }
 
 }
