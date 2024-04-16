61034dd3e0db85d030ebc13ce40fa9ae7e4247bd
msiddique12 <67293796+msiddique12@users.noreply.github.com>
2024-04-16 16:29:32 -0500

    Removed 3 folders

--- Client/Nandini/Ellipse1Icon.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Ellipse1Icon = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 40 40' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <circle cx={20} cy={20} r={20} fill='#A3B18A' />
-  </svg>
-);
-const Memo = memo(Ellipse1Icon);
-export { Memo as Ellipse1Icon };
