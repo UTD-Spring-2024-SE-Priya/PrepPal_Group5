70939e0e8bf5f463a12755ff804dfec62bb4c4d2
Nandinik12 <86893957+Nandinik12@users.noreply.github.com>
2024-04-18 10:27:57 -0500

    Delete Client/nandinik12_s_application3-v1.0.1 directory

--- Client/nandinik12_s_application3-v1.0.1/src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Button } from "./Button";
-import { Img } from "./Img";
-import { Input } from "./Input";
-import { SelectBox } from "./SelectBox";
-export { Text, Heading, Button, Img, Input, SelectBox };
