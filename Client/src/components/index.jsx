4750304027a8c40155f7510c12ec6aa6c1174fb9
meglammata26 <118643010+meglammata26@users.noreply.github.com>
2024-04-18 15:01:40 -0500

    Delete Client/src directory

--- Client/src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Img } from "./Img";
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { Input } from "./Input";
-import { Button } from "./Button";
-export { Img, Text, Heading, Input, Button };
